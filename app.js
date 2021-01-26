fea156a873dabd1aa2ce594ca386daca8812020f
Rebecca Bakels <rebeccabakels@gmail.com>
2021-01-26 14:43:15 -0500

    added update or PATCH call

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { getEvents, postEvent } = require('./src/events')
 const { getPeople, postPerson } = require('./src/users')
-const { getSingleEvent, deleteEvents } = require('./src/events/eventId')
+const { getSingleEvent, deleteEvent, updateEvent } = require('./src/events/eventId')
 const { getSinglePerson } = require('./src/users/userId')
 
 const app = express()
@@ -13,7 +13,8 @@ const port = 3001
 app.get('/events', getEvents)
 app.get('/events/:eventId', getSingleEvent)
 app.post('/events', postEvent)
-app.delete('/events/:eventId', deleteEvents)
+app.delete('/events/:eventId', deleteEvent)
+app.patch('/events/:eventId', updateEvent)
 
 app.get('/people', getPeople)
 app.get('/people/:userId', getSinglePerson)
--- src/events/eventId.js
+++ src/events/eventId.js
@@ -2,64 +2,94 @@ const admin = require("firebase-admin");
 const serviceAccount = require("../../wildhabitatexercise-firebase-adminsdk-z62ei-63967fed83.json");
 
 if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+const firestore = admin.firestore();
+const eventsRef = firestore.collection("events");
+
+exports.getSingleEvent = (req, res) => {
+  if (!firestore) {
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount)
+      credential: admin.credential.cert(serviceAccount),
+    });
+    firestore = admin.firestore();
+  }
+  const { eventId } = req.params;
+  eventsRef
+    .doc(eventId)
+    .get()
+    .then((doc) => {
+      let event = doc.data();
+      event.id = doc.id;
+      res.status(200).json({
+        status: "success",
+        data: event,
+        message: "Events loaded successfully",
+        statusCode: 200,
+      });
     })
-  }
-  const firestore = admin.firestore()
-  const eventsRef = firestore.collection('events')
-  
-  exports.getSingleEvent = (req, res) => {
-    if(!firestore) {
-      admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
-      })
-      firestore = admin.firestore()
-    }
-    const { eventId } = req.params
-    eventsRef.doc(eventId).get()
-      .then(doc => {
-        let event = doc.data()
-        event.id = doc.id
-        res.status(200).json({
-          status: 'success',
-          data: event,
-          message: 'Events loaded successfully',
-          statusCode: 200
-        })
-      })
-      .catch(err => {
-        res.status(500).send({
-          status: 'error',
-          data: err,
-          message: 'Error getting events',
-          statusCode: 500
-        })
-      })
-  }
+    .catch((err) => {
+      res.status(500).send({
+        status: "error",
+        data: err,
+        message: "Error getting events",
+        statusCode: 500,
+      });
+    });
+};
 
-  exports.deleteEvent = (reg, res) => {
-    if(!firestore) {
-        admin.initializeApp({
-          credential: admin.credential.cert(serviceAccount)
-        })
-        firestore = admin.firestore()
-      }
-      eventsRef.doc(req.params.eventId).delete()
-      .then(() => {
-          res.status(200).json({
-            status: 'success',
-            message: 'Events DELETED',
-            statusCode: 200 
-          })
-      })
-      .catch(err => {
-          res.status(500).json({
-            status: 'error',
-            data: err,
-            message: 'Error deleting events',
-            statusCode: 500
-          })
-      })
-
+exports.deleteEvent = (req, res) => {
+  if (!firestore) {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+    firestore = admin.firestore();
   }
+  eventsRef
+    .doc(req.params.eventId)
+    .delete()
+    .then(() => {
+      res.status(204).json({
+        status: "success",
+        message: "Events DELETED",
+        statusCode: 204,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        status: "error",
+        data: err,
+        message: "Error deleting events",
+        statusCode: 500,
+      });
+    });
+};
+
+exports.updateEvent = (req, res) => {
+  if (!firestore) {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+    firestore = admin.firestore();
+  }
+  eventsRef
+    .doc(req.params.eventId)
+    .update(req.body)
+    .then(() => {
+      res.status(200).json({
+        status: "item updated",
+        message: "Events updated",
+        statusCode: 200,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        status: "error",
+        data: err,
+        message: "Error updating event",
+        statusCode: 500,
+      });
+    });
+};
